b400f1de71f5a072fa1190ec98533bdcee9a44c8
Eduards Sniedze <eduards.sniedze@gmail.com>
2020-11-23 22:08:42 +0200

    Product(2)

--- src/routes/product.js
+++ src/routes/product.js
@@ -3,10 +3,22 @@ const { adminMiddleware, requireSignin } = require('../common-middleware');
 const { createProduct } = require('../controller/product');
 // const {  } = require('../controller/category');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
 const router = express.Router();
+const shortid = require('shortid');
+const path = require('path');
 
-router.post('/product/create', requireSignin, adminMiddleware, upload.single('productPictures'), createProduct);
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(path.dirname(__dirname), 'uploads'))
+    },
+    filename: function (req, file, cb) {
+        cb(null, shortid.generate() + '-' + file.originalname)
+    }
+});
+
+const upload = multer({ storage });
+
+router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
 
 // router.get('/category/getcategory', getCategories);
 
